486360b7f76c73cfb36f7c83e92e3124923915e5
Justin Darmody <jdarmody83@gmail.com>
2025-08-10 06:53:39 +1000

    new unit test project

---
+++ src/EsriDemo.UnitTests/UnitTestBase.cs
@@ -0,0 +1,25 @@
+﻿using Moq.AutoMock;
+using System.Globalization;
+
+namespace EsriDemo.UnitTests;
+
+public abstract class UnitTestBase<TSut> : UnitTestBase where TSut : class
+{
+    protected UnitTestBase()
+    {
+        Sut = Mocker.CreateInstance<TSut>();
+    }
+
+    protected TSut Sut { get; }
+}
+
+public abstract class UnitTestBase
+{
+    protected UnitTestBase()
+    {
+        CultureInfo.CurrentCulture = new CultureInfo("en-AU");
+        Mocker = new AutoMocker();
+    }
+
+    protected AutoMocker Mocker { get; }
+}
