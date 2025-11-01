5a6f299e3a334da4d70557d367cf2414c6fca9ba
Justin Darmody <jdarmody83@gmail.com>
2025-11-01 15:16:29 +1100

    MauiProgram additions

--- src/EsriDemo/MauiProgram.cs
+++ src/EsriDemo/MauiProgram.cs
@@ -1,6 +1,17 @@
 ﻿using CommunityToolkit.Maui;
 using EsriDemo.Core.Logging.Firebase;
 using EsriDemo.Core.Analytics;
+using EsriDemo.Core.Alerts;
+using EsriDemo.Core.Biometrics;
+using EsriDemo.Core.Navigation;
+using EsriDemo.Core.Logging;
+using EsriDemo.Features.Root;
+using EsriDemo.Core.Effects;
+#if ANDROID
+using EsriDemo.Droid.Handlers;
+#elif IOS
+using EsriDemo.iOS.Handlers;
+#endif
 
 namespace EsriDemo;
 
@@ -18,11 +29,19 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
-
-#if DEBUG
-		builder.Logging.AddDebug();
+			})
+			.ConfigureEffects(effects =>
+			{
+				effects.Add<SafeAreaInsetEffect, SafeAreaInsetPlatformEffect>();
+			})
+			.ConfigureMauiHandlers(handlers =>
+			{
+#if ANDROID
+				handlers.AddHandler(typeof(TabbedPage), typeof(CustomTabbedPageHandler));
+#elif IOS
+				handlers.AddHandler(typeof(TabbedPage), typeof(CustomTabbedRenderer));
 #endif
+			});
 
 		return builder.Build();
 	}
@@ -30,20 +49,22 @@ public static class MauiProgram
 	private static MauiAppBuilder ConfigureLogging(this MauiAppBuilder builder)
 	{
 #if DEBUG
-		builder.Logging.AddDebug();
+		//builder.Logging.AddDebug();
+		builder.Logging.ClearProviders();
+		builder.Logging.AddProvider(new TimestampDebugLoggerProvider());
 		builder.Logging.SetMinimumLevel(LogLevel.Debug);
 #else
 		builder.Logging.AddProvider(new FirebaseLoggerProvider(LogLevel.Warning));
-// #if DEV
-// 		// Using Console which will log to iOS NSLog and Android Logcat for test releases
-// 		builder.Logging.AddConsole();
-// 		builder.Logging.SetMinimumLevel(LogLevel.Debug);
-// #elif TEST
-// 		// Using Console which will log to iOS NSLog and Android Logcat for test releases
-// 		builder.Logging.AddConsole();
-// #endif
+#if DEV
+		// Using Console which will log to iOS NSLog and Android Logcat for test releases
+		builder.Logging.AddConsole();
+		builder.Logging.SetMinimumLevel(LogLevel.Debug);
+#elif TEST
+		// Using Console which will log to iOS NSLog and Android Logcat for test releases
+		builder.Logging.AddConsole();
 #endif
-		return builder; 
+#endif
+		return builder;
 	}
 
 	private static MauiAppBuilder UseFirebase(this MauiAppBuilder builder)
@@ -70,4 +91,43 @@ public static class MauiProgram
         
         return builder;
     }
+
+	private static MauiAppBuilder RegisterEssentials(this MauiAppBuilder builder)
+    {
+	    var services = builder.Services;
+
+	    // Register ones needed by the app
+	    services.AddSingleton<IBrowser>(_ => Browser.Default);
+        services.AddSingleton<IClipboard>(_ => Clipboard.Default);
+        services.AddSingleton<IConnectivity>(_ => Connectivity.Current);
+        services.AddSingleton<ILauncher>(_ => Launcher.Default);
+        services.AddSingleton<IPhoneDialer>(_ => PhoneDialer.Default);
+        services.AddSingleton<IPreferences>(_ => Preferences.Default);
+        services.AddSingleton<ISecureStorage>(_ => SecureStorage.Default);
+
+		return builder;
+    }
+
+	private static MauiAppBuilder RegisterServices(this MauiAppBuilder builder)
+    {
+		var services = builder.Services;
+#if DEBUG
+		services.AddSingleton<IAnalytics, DebugAnalytics>();
+#endif
+		services.AddSingleton<IAlertService, AlertService>();
+		services.AddSingleton<IBiometricsService, BiometricsService>();
+		services.AddTransient<INavigationService, NavigationService>();
+		services.AddSingleton<IPopupNavigationService, PopupNavigationService>();
+		
+		//TODO API clients
+		return builder;
+	}
+
+	private static MauiAppBuilder RegisterPages(this MauiAppBuilder builder)
+    {
+		var services = builder.Services;
+		services.AddTransient<RootPage>();
+		//services.AddTransient<Feature1Page, Feature1PageModel>();
+		return builder;
+	}
 }
